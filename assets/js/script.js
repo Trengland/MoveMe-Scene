969d0f26adac3c0b595ad79097efacfd88c49954
Chelsea-Luevano <120981491+ChelseaLuevano@users.noreply.github.com>
2023-03-11 11:11:55 -0500

    was able to updated Youtube link to work correctly in trailter list links

--- assets/js/script.js
+++ assets/js/script.js
@@ -105,7 +105,7 @@ function Trailers (movieID) {
                 // Creates trailer Link list Element
                 let li1 = document.createElement("li");
                 // Add text to link
-                // li1.innerHTML = '<a href="YoutubeLink">Watch the trailer video!</a>'
+                li1.innerHTML ='<a href=' +YoutubeLink + '>Watch the trailer video!</a>'
                 // li1.textContent = "Watch the trailer video!"
                 // Update href link with new YoutubeLink Variable
                 // li1.href = "YoutubeLink";
@@ -114,7 +114,6 @@ function Trailers (movieID) {
                 // Append list items to ordered trailerListEl
              
                 trailerListEl.appendChild(li1);
-                
             
                 // resultsEl.append(linkTrailerEl.href = YoutubeLink);
 
