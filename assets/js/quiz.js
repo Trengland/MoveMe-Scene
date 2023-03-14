9b2a692639c457fdf42613976364bad60385432f
Chelsea-Luevano <120981491+ChelseaLuevano@users.noreply.github.com>
2023-03-14 12:17:51 -0400

    was able to emded trailer videos directly into the quiz html by updated the trailer function

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,7 +1,7 @@
 
 // Starter Variables
 let recommendedMovie = "";
-// let movieID = "";
+let movieSection;
 
 // API URL Variables
 let apiKeyTMBD = "87ceec9af92ce89acfb2e11778f0841f";
@@ -12,6 +12,9 @@ let trailerSourceEl = document.getElementById('src');
 let trailerVideoEl = document.getElementById('video');
 let trailerEl = document.getElementById('link-to-trailer');
 let recommendedTitleEl = document.getElementById("recommended-title");
+let resultsSectionEl = document.getElementById("results");
+
+
 
 
 // Variables defined to show & hide HTML elements on page
@@ -103,6 +106,7 @@ $(function () {
 			$("#results").append("<div class='pad-8'><img src='https://image.tmdb.org/t/p/original" + poster + "' class='poster'/></div>");
 			$("#results").append("<div class='pad-8'>#" + count + ": " + title + "<span class='margin-left-10 small-text'>Released: " + dayjs(releaseDate).format('MM/DD/YYYY') + "</span></div>");
 			$("#results").append("<div class='pad-8'>" + overview + "</div>");
+
 			// SM - Thumbs up and down buttons
 			$("#results").append("<div class='pad-8 is-centered buttons'><button class='button thumbs-up-btn' id='thumbsUpBtn'><span class='icon'><i class='fa-solid fa-thumbs-up'></i></span></button><button class='button thumbs-down-btn' id='thumbsDownBtn'><span class='icon'><i class='fa-solid fa-thumbs-down'></i></span></button></div><hr class='hr'>");
 			console.log("title" + title);
@@ -134,64 +138,97 @@ function getMovieID (title){
 
 // Function to populate Trailer with movie ID
 function Trailers (movieID) {
-    let trailerURL = "https://api.themoviedb.org/3/movie/" + movieID + "/videos?api_key=" + apiKeyTMBD + "&language=en-US";
+    // let trailerURL = "https://api.themoviedb.org/3/movie/" + movieID + "/videos?api_key=" + apiKeyTMBD + "&language=en-US";
     // Chelsea's back up plan if trailerURL doesn't work
-    // let videoURL = "https://api.themoviedb.org/3/movie/" + movieID + "?api_key=" + apiKeyTMBD + "&append_to_response=videos,images";
+    let videoURL = "https://api.themoviedb.org/3/movie/" + movieID + "?api_key=" + apiKeyTMBD + "&append_to_response=videos,images";
    
-    fetch(trailerURL)
+    fetch(videoURL)
     .then(function(response){
             if (response.ok) {
                 response.json()
 				.then(function (data) {
                 	console.log(data);	
-					createTrailerElement(data);
+					// createTrailerElement(data);
+					// playVideo(data);
+					if (data.videos.results.length != 0) {
+						let youTubeKey = data.videos.results[0].key;
+						let trailerListEl = document.querySelector("#trailer-list");
+						let frame =  document.createElement("iframe");
+						frame.setAttribute ("src","https://www.youtube.com/embed/" + youTubeKey );
+						frame.setAttribute("name", "Trailer Video");
+						frame.setAttribute("id", "frame");
+						frame.frameborder = 0;
+						frame.width = 100 + "%";
+						frame.height = 100 + "%";
+						trailerListEl.append(frame);
+					}	 else {	
+						// Creates paragraph element
+						let noVideosFound = document.createElement("p");
+						let trailerListEl = document.querySelector("#trailer-list");
+
+						// Add text to paragraph element
+						noVideosFound.textContent= 'Unfortunately, there are no video trailers for the movie' 
+
+						// Append paragraph element to trailerListEl
+						trailerListEl.append(noVideosFound);	
+						// trailerListEl.append("No Trailer");
+						}
 				});
 			} else {
-				console.log("error");	
+				console.log("error");
+				
 			}  
 		}); 	
 };	   
 
 // Function to create an element under Video Trailers if the trailers API call was successful
-function createTrailerElement (data) {
-	console.log(data);
-	if (data.results.length === 0) {
-		console.log(data.results)
+ // function createTrailerElement (data) { 
+// 	console.log(data);
+// 	if (data.results.length === 0) {
+// 		console.log(data.results)
 
-		// Creates paragraph element
-		let noVideosFound = document.createElement("p");
+// 		// Creates paragraph element
+// 		let noVideosFound = document.createElement("p");
 
-		// Add text to paragraph element
-		noVideosFound.textContent= 'Unfortunately, there are no video trailers for the movie' 
+// 		// Add text to paragraph element
+// 		noVideosFound.textContent= 'Unfortunately, there are no video trailers for the movie' 
 
-		// Append paragraph element to trailerListEl
-		trailerEl.appendChild(noVideosFound);
-	}
+// 		// Append paragraph element to trailerListEl
+// 		trailerEl.append(noVideosFound);
+// 	}
 
-	else {
-		// Pull they video key from the API Trailer Array
-		let videoKey = data.results[0].key;	
+// 	else {
+// 		// Pull they video key from the API Trailer Array
+// 		let videoKey = data.results[0].key;	
 
-		let trailerMovieName =data.results[0].name;
-		console.log(trailerMovieName);
-		//  Create the Youtube Link with the key of the video
-		let YoutubeLink = "https://www.youtube.com/watch?v=" + videoKey;
-		console.log(YoutubeLink);
+// 		let trailerMovieName =data.results[0].name;
+// 		console.log(trailerMovieName);
+// 		//  Create the Youtube Link with the key of the video
+// 		let YoutubeLink = "https://www.youtube.com/watch?v=" + videoKey;
+// 		console.log(YoutubeLink);
 
-		// Find organized list element
-		let trailerListEl = document.querySelector("#trailer-list");
+// 		// Find organized list element
+// 		let trailerListEl = document.querySelector("#trailer-list");
 
-		// Creates trailer Link list Element
-		let li1 = document.createElement("li");
-		// li1 = document.setAttribute("id", "list-item")
+// 		// Creates trailer Link list Element
+// 		let li1 = document.createElement("li");
 
-		// // Add text to link
-		li1.innerHTML ='<a href=' +YoutubeLink + '>Watch the trailer video: '+ trailerMovieName + '</a>'
+// 		// // Add text to link
+// 		li1.innerHTML ='<a href=' +YoutubeLink + '>Watch the trailer video: '+ trailerMovieName + '</a>'
 
-		// Append list items to ordered trailerListEl
-		trailerListEl.appendChild(li1);
-	}
-};	
+// 		// Append list items to ordered trailerListEl
+// 		trailerListEl.appendChild(li1);
+// 	}
+// };	
+
+// function playVideo(){
+// if (response.videos.results.length != 0) {
+// 	let youTubeKey = response.videos.results[0].key;
+// 	$("#results").append("<div class='pad-8'><iframe width='560' height='315' src='https://www.youtube.com/embed/" + youTubeKey + "' frameborder='0' allowfullscreen></iframe></div><hr class='hr'>");
+// }	 else {	
+// 	$("#results").append("<div class='pad-8 big-text red-text'>No Trailer</div><hr class='hr'>");
+// 	}
+// }
 
 // Hide the quiz questions when results are displayed
 function hideQuiz() {
@@ -231,10 +268,11 @@ $(document).ready(function() {
   console.log("test");
   });
   
+  
   $(".modal-background").click(function () {
   $("#sign-up-modal").removeClass("is-active");
   });
-
+	
   // SM - Button Click Events for Thumbs Up and Down
 $("#thumbsUpBtn").click(function () {
 	
