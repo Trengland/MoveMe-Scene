55845f4ef17a49c52c174da1c07bcb969289d8ce
smilillo <sofiemilillo@gmail.com>
2023-03-14 18:59:59 -0400

    commit before pull

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -195,6 +195,31 @@ function Trailers (movieID) {
 		}); 	
 };	
 
+// CG - Displaying results in modal of info pulled from api
+$.ajax(settings).done(function (response) {
+	console.log(response);
+	var modal = document.getElementById("myModal");
+	var btn = document.getElementById("myBtn");
+	btn.onclick = function() {
+		modal.style.display = "block";}
+	$("#movieQuote").append(response.quote);
+	$("#movie").append(response.movie);
+});
+
+// CG - Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+myModal.style.display = "none";
+}
+
+// CG - Close Modal 
+window.onclick = function (event) {
+if (event.target == myModal) {
+myModal.style.display = "none";
+}
+}
+
 // SM - Mobile Menu -- Code from Bulma documentation example js
 $(document).ready(function() {
 
